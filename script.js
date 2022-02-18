27720e79214292d559e6cff6aee906ac32653b7e
Trenton <dr.teasdale.trenton@gmail.com>
2022-02-17 20:35:24 -0700

    update

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const game = (() => {
                 gameTile.addEventListener('click', function(){
                     player.play(gameTile);
                     board.createBoard();
+                    setTimeout(function() {test();}, 500);
+                    
                     pcTile(gameTile);
                     PC.play(pcChoice);
                     setTimeout(function() {board.createBoard()}, 500);
@@ -26,8 +28,47 @@ const game = (() => {
                 }
         }
     };
+    // 1  2  3
+    // 4  5  6
+    // 7  8  9
+    //TEST LOGIC 
+    function test() {
+        let current= game.board['array'];
+        function end(){
+            player.winner = true;
+            alert('player wins');
+            board.array = ['','','','','','','','','']
+            board.createBoard();
+        }
+        if (current[0] == 'x' && current[1] == 'x' && current[2] == 'x') {
+            end();
+        }
+        if (current[3] == 'x' && current[4] == 'x' && current[5] == 'x') {
+            end();
+        }
+        if (current[6] == 'x' && current[7] == 'x' && current[8] == 'x') {
+            end();
+        }
+        if (current[2] == 'x' && current[4] == 'x' && current[6] == 'x') {
+            end();
+        }
+        if (current[0] == 'x' && current[4] == 'x' && current[8] == 'x') {
+            end();
+        }
+        if (current[0] == 'x' && current[3] == 'x' && current[6] == 'x') {
+            end();
+        }
+        if (current[1] == 'x' && current[4] == 'x' && current[7] == 'x') {
+            end();
+        }
+        if (current[2] == 'x' && current[5] == 'x' && current[9] == 'x') {
+            end();
+        }
+    }
+    // Random PC Logic 
     function pcTile(gameTile) {
-        const gameBoard = document.querySelector('.board');
+        if (player.winner == false){
+            const gameBoard = document.querySelector('.board');
         let choice = Math.floor(Math.random() * 9);
         let limit = 0;
         let valid = false;
@@ -49,21 +90,25 @@ const game = (() => {
             valid = true;
             return {pcChoice};
         }
+        }
+    } else {
+        //don't play if the game is over
+        pcChoice = undefined;
     }
-        // if (pcChoice == gameTile) {
-        //     pcChoice = (gameBoard.children [choice + 1]);
-        // } else if (pcChoice == gameTile && choice == 9) {
-        //     pcChoice = (gameBoard.children [choice - 1]);
-        // }
     }
+    //create first blank array
     board.createBoard();
+    //RESET BUTTON
     const reset = document.querySelector('.reset')
     reset.addEventListener('click', function(){
         board.array = ['','','','','','','','','']
         board.createBoard();
     })
+    //CREATE PLAYER OBJECTS
     const makePlayer = (name, marker) => {
+        let winner = false;
         const play = (gameTile) => {
+            player.winner = false;
             let position = game.board['array'];
             if (position[gameTile.id-1] == 'x' || position[gameTile.id-1] ==  'o') {
                 //do nothing
@@ -74,23 +119,20 @@ const game = (() => {
         return {
             name,
             marker,
+            winner,
             play
         }
     }
+    //CREATE PC PLAYER WITH PC LOGIC
     const computer = () => {
         const prototype = makePlayer('PC', 'o');
         const choseTile = () => pcTile(gameTile);
         return Object.assign({}, prototype, {choseTile});
     };
+    //PLAYERS TO BE CREATED
     const PC = computer('PC', 'o');
     const player = makePlayer ('Tre', 'x');
     return {
         board
     };
 })();
-
-
-
-// 1  2  3
-// 4  5  6
-// 7  8  9
